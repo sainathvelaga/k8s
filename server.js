c610e1c436e307301518ef10d92aaa22ba926920
omkarsuperb <67182857+omkarsuperb@users.noreply.github.com>
2022-04-27 21:23:53 +0530

    added k8s

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world\n');
+  res.send('Hello From K8s\n');
 });
 
 app.listen(PORT);
