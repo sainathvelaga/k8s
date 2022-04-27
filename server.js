1218003b332a70229bf360d8cd747d5dfb910490
omkarsuperb <67182857+omkarsuperb@users.noreply.github.com>
2022-04-27 21:38:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello From K8s\n');
+  res.send('I love you Shruti\n');
 });
 
 app.listen(PORT);
