c6176234d9af5d0e14d6728cb1745e47ff1a79b1
Rushabh Mahale <63963025+rushabhmahale@users.noreply.github.com>
2022-04-29 14:43:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.get('/', function (req, res) {
   res.send('Hello from jenkins\n');
-  document.body.style.backgroundColor = "aqua";
 });
+
+// change background color for whole body..
+function changebackground(){
+	document.body.style.backgroundColor = 'aqua';
+}
+ 
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
