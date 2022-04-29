7ecc9d26fdaf6f28c2393f19f5554acfad7aa4f3
Rushabh Mahale <63963025+rushabhmahale@users.noreply.github.com>
2022-04-29 15:03:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello from jenkins\n');
+  res.send('Hello from Kubernetes\n');
 });
 
 // change background color for whole body..
